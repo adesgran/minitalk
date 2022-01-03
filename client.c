93ead44579cb156bd4147457e99388530af6a53b
adesgran <adesgran.dev@gmail.com>
2022-01-03 17:47:39 +0100

    Client can send string to the Server

--- client.c
+++ client.c
@@ -6,32 +6,61 @@
 /*   By: adesgran <adesgran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 14:47:07 by adesgran          #+#    #+#             */
-/*   Updated: 2022/01/03 16:20:10 by adesgran         ###   ########.fr       */
+/*   Updated: 2022/01/03 17:33:57 by adesgran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minitalk.h>
 
-void	handle_sigout(int sig)
+static char	*convert_base(char c)
 {
-	ft_printf("Signal out : %d\n:", sig);
-	if (sig == 2)
+	int		i;
+	char	*res;
+
+	res = malloc(sizeof(char) * 8);
+	i = 8;
+	while (i)
 	{
-		ft_printf("Exit !\n");
-		exit(0);
+		if (c % 2)
+			res[i - 1] = SIGUSR2;
+		else
+			res[i - 1] = SIGUSR1;
+		c = c / 2;
+		i--;
 	}
+	return (res);
+}
+
+static void send_char(char c, pid_t pid)
+{
+	char	*str;
+	int		i;
+
+	str = convert_base(c);
+	i = 0;
+	while (i < 8)
+	{
+		kill(pid, str[i]);
+		i++;
+	}
+	free(str);
 }
 
 int	main(int ac, char **av)
 {
 	pid_t	pid;
+	char	*to_send;
 
 	if (ac < 2)
 		return (1);
 	pid = ft_atoi(av[1]);
-	signal(ft_atoi(av[2]), handle_sigout);
-	kill(pid, ft_atoi(av[2]));
-		
+	to_send = av[2];
+	while (*to_send)
+	{
+		send_char(*to_send, pid);
+		to_send++;
+	}
+	send_char('\n', pid);
 	(void)ac;
 	(void)av;
 	return (0);
--- server.c
+++ server.c
@@ -6,29 +6,76 @@
 /*   By: adesgran <adesgran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 14:48:27 by adesgran          #+#    #+#             */
-/*   Updated: 2022/01/03 16:00:13 by adesgran         ###   ########.fr       */
+/*   Updated: 2022/01/03 17:38:45 by adesgran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minitalk.h>
 
-void	handle_sigint(int sig)
+static void	print_char(char *str)
 {
-	ft_printf("Signal : %d\n:", sig);
-	if (sig == 2)
+	char	c;
+	int		i;
+
+	i = 0;
+	c = 0;
+	while (i < 8)
 	{
-		ft_printf("Exit !\n");
-		exit(0);
+		c *= 2;
+		if (str[i] == SIGUSR2)
+			c += 1;
+		i++;
 	}
+	ft_printf("%c", c);
+}
+
+static void	read_input(char c)
+{
+	static int	i;
+	static char	*str;
+
+	if (!i || i == 8)
+		i = 0;
+	if (!str)
+	{
+		str = malloc(sizeof(char) * 8);
+		if (!str)
+			return ;
+	}
+	str[i] = c;
+	if (i == 7)
+	{
+		print_char(str);
+		free(str);
+		str = NULL;
+	}
+	i++;
+}
+
+static void	handle_sigstop(int sig)
+{
+	ft_printf("Stop Server\n");
+	(void)sig;
+}
+
+static void	handle_sigusr1(int sig)
+{
+	read_input((char)sig);
+}
+
+static void	handle_sigusr2(int sig)
+{
+	read_input((char)sig);
 }
 
 int	main(void)
 {
-	signal(SIGINT, handle_sigint);
+	signal(SIGSTOP, handle_sigstop);
+	signal(SIGUSR1, handle_sigusr1);
+	signal(SIGUSR2, handle_sigusr2);
 	ft_printf("PID : %d\n", getpid());
-	for (int i = 0; i < 10; i++)
+	while (1)
 	{
-		ft_printf("Waiting for Signal..\n");
 		sleep(5);
 	}
 	return (0);
